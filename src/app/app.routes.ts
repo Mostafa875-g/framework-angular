29bbdd1ad680ede0630b226d033231594e749910
Mostafa Ayman <elmostaf36@gmail.com>
2025-08-02 21:05:32 +0300

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Home } from './components/home/home';
+import { Notfound } from './components/notfound/notfound';
+import { Portfolio } from './components/portfolio/portfolio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:"full"},
+    {path:'home', component:Home, title:'Start Framework'},
+    {path:'about', component:About, title:'About - Start Framework'},
+    {path:'contact', component:Contact, title:'Contact - Start Framework'},
+    {path:'portfolio', component:Portfolio, title:'Portfolio - Start Framework'},
+    {path:'**', component:Notfound, title:'Not Found Page'},
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Navbar } from "./components/navbar/navbar";
+import { Notfound } from "./components/notfound/notfound";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar, Footer, Notfound],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
